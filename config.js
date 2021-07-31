39fe61178fcdebe3ccbf5c381c825f4f58ac1d18
AUnityProgrammer <80798038+AUnityProgrammer@users.noreply.github.com>
2021-07-31 21:51:19 +0700

    Update config.js

--- config.js
+++ config.js
@@ -51,7 +51,7 @@ const CONFIG = {
       id: '3',
       name: 'Todoist',
       icon: 'trello',
-      link: 'https://calendar.google.com/calendar/r',
+      link: 'https://elearning-smplabsky.labschool-unj.sch.id/my/',
     },
     {
       id: '4',
